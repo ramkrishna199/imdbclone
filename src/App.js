c0709ed71b83af984534acd2bc6bc0b257acce8b
Ramkrishna Mahato <109056430+ramkrishna199@users.noreply.github.com>
2023-03-20 13:00:29 +0530

    last update

--- src/App.js
+++ src/App.js
@@ -11,9 +11,11 @@ function App() {
       <Router>
         <Header />
         <Routes>
+     <Route index element={<Home />}></Route>
           <Route path="/" element={<Home />}></Route>
           <Route path="movie/:id" element={<Movie />}></Route>
           <Route path="movies/:type" element={<MovieList />}></Route>
+ <Route path="*" element={<Home />}></Route>
         </Routes>
       </Router>
     </div>
